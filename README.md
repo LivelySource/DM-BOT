e8b1879e02809c92a26f07a79f6190829f458137
LivelySource <weston.schmidt4@gmail.com>
2018-12-07 19:05:45 -0800

    Publication 📧

--- README.md
+++ README.md
@@ -1,1 +1,48 @@
-# DM BOT
+<div>
+  <div style="margin-left:auto;margin-right:auto;">
+    <img src="https://cdn.discordapp.com/attachments/502649544622735362/520794535018627083/dm-bot.png"><br><br>
+</div>
+
+Hello glad you stumpled upon DM-BOT. You can freely modify, configure, produce this code. I only wish you respect my wishes and give credit where it is due. Thank you! If you have any questions or concerns visit my webpage > [LivelySource.tk](https://livelysource.tk)
+
+## Requirements
+
+- `git` command line ([Windows](https://git-scm.com/download/win)|[Linux](https://git-scm.com/book/en/v2/Getting-Started-Installing-Git)|[MacOS](https://git-scm.com/download/mac)) installed
+- `node` [Version 8.0.0 or higher](https://nodejs.org)
+
+You'll need your bot's token. This is obtained by creating an application in
+the Developer section of discordapp.com. 
+
+## Installing DM-BOT
+
+Run this command within your folder.
+You can simply shift + left-click to open the Powershell window.
+
+`git clone https://github.com/livelysource/dm-bot.git`
+
+then... 
+
+- In the folder where you used the git command, run `npm install` to install it's dependencies,
+- Rename  `config-example.json` to `config.json`
+- Edit `config.json` and fill in all the required inputs.
+
+## Using DM- BOt
+
+To start DM-BOT, in command prompt or powershell(whichever you are using), run the following command:
+`npm start`
+If you wish to publish this on github, besure to include `config.json` in your `.gitignore` file.
+
+## To Invite Your Bot!
+
+To add the bot to your guild, you have to get an oauth link for it. 
+
+You can use this site to help you generate a full OAuth Link, which includes a calculator for the permissions:
+[Permission Calculator](https://finitereality.github.io/permissions-calculator/?v=0)
+
+## Understanding How it Works
+
+DM-Bot is using the [discord.js](discord.js.org) lib.
+DM-Bot takes messages from the dm channel, then sends them to a server's specific channel.
+
+To there you can reply to the message by copying the command's syntax provided. There you can fill in the `<message>` 
+area!
---
+++ dmBot.js
@@ -0,0 +1,76 @@
+//       _ _           _                                    
+//      | (_)_   _____| |_   _ ___  ___  _   _ _ __ ___ ___ 
+//      | | \ \ / / _ \ | | | / __|/ _ \| | | | '__/ __/ _ \
+//      | | |\ V /  __/ | |_| \__ \ (_) | |_| | | | (_|  __/
+//      |_|_| \_/ \___|_|\__, |___/\___/ \__,_|_|  \___\___|.tk
+//                       |___/                              
+const Discord = require("discord.js");
+const dmBot = new Discord.Client();
+const config = require("./config.json");
+
+// Edit as you please~ Besure to credit the writeful creators.
+
+dmBot.on("ready", async () => {
+
+    console.log(config.READY_MESSAGE);
+    dmBot.user.setActivity(config.ACTIVITY_STATUS, {
+        type: "WATCHING"
+    });
+
+});
+
+dmBot.on("message", (message) => {
+    
+    
+    if (message.channel.type === "dm") { 
+        var args = message.content.split(" ").slice(0)
+        var args = args.slice(0).join(" ")
+        var BOT_ID = dmBot.user.id
+        var userID = message.author.id
+        if (message.content.startsWith(config.PREFIX)) return message.channel.send(":x: Please use commands in real server! :x:") //if the message is a command
+        if (message.author.bot) return;
+        message.channel.send("This message has been send to the staff! :incoming_envelope:").then(msg => msg.delete(3000))
+        if (message.content.startsWith(config.PREFIX)) return
+        if (args.length > 256) return message.reply("Your message content too many characters :/") //if the message contnt more than 256 character, what fields do not allow
+        var embed = new Discord.RichEmbed()
+            .setColor(config.ORANGE)
+            .setAuthor("New Message", "https://cdn.discordapp.com/attachments/502649544622735362/520740243133956138/receive.png")
+            .addBlankField(true)
+            .addField(`Sent by: ${message.author.username}`, 
+            args)
+            .addBlankField(true)
+            .setTitle("*Message**:")
+            .setFooter("This Message Was Sent By: " + message.author.username + " ", message.author.avatarURL)
+            .setTimestamp()
+        dmBot.guilds.get(config.SERVER_ID).channels.get(config.CHANNEL_ID).send(embed).catch(console.log(`Message recieved from ${userID}!(${message.author.username})`))
+        dmBot.guilds.get(config.SERVER_ID).channels.get(config.CHANNEL_ID).send({embed: {
+            "description": `${config.PREFIX}reply ${message.author.id} <message>`,
+        }
+    })
+    }else
+    
+    
+    if (message.content.startsWith(config.PREFIX + "reply")) {
+        if (message.author.id !== config.YOUR_ID) return message.reply('You cannot use that!')
+        var args = message.content.split(" ").slice(0)
+        var Rargs = message.content.split(" ").slice(2).join(" ")
+        var userID = args[1]
+        if (isNaN(args[1])) return message.reply("This is not an ID! Make sure to you the user's ID!") //if args is Not A Number!
+        var embed = new Discord.RichEmbed()
+            .setColor(config.ORANGE)
+            .setAuthor("New Message", "https://cdn.discordapp.com/attachments/502649544622735362/520740243133956138/receive.png")
+            .addBlankField(true)
+            .setDescription(Rargs)
+            .addBlankField(true)
+            .setTitle("**Message**:")
+            .setFooter("This Message Was Sent By: " + message.author.username + " ", message.author.avatarURL)
+        dmBot.users.get(userID).send(embed).catch(console.log(`Message was sent to ${userID}!`))
+        if (message.author.bot) return;
+        message.channel.send("Your Message was Sent!").then(msg => msg.delete(3000)).catch(console.error)
+
+    }
+
+});
+
+
+dmBot.login(config.TOKEN);
